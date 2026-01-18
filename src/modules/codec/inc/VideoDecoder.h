b498fff5e7d5a4fbf598979bf674a9089b4ce2b7
SwimmingLee <swimming7291@gmail.com>
2026-01-18 15:58:46 +0900

    feat: Implement video decoding functionality with `IDecoder` interface and `VideoDecoder` class.

--- src/modules/codec/inc/VideoDecoder.h
+++ src/modules/codec/inc/VideoDecoder.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#include "IDecoder.h"
+#include "Channel.h"
+#include "AVMemory.h"
+#include <map>
+
 class VideoDecoder final : public IDecoder {
 public:
     VideoDecoder() = default;
     ~VideoDecoder() = default;
 
-    virtual bool initialize(shared_ptr<Channel<AVPacketPtr>> src, shared_ptr<Channel<AVFramePtr>> dst) override;
+    virtual bool initialize(DecoderParam& param, shared_ptr<Channel<AVPacketPtr>> src, shared_ptr<Channel<AVFramePtr>> dst) override;
     virtual void run() override;
     virtual void stop() override;
+
+private:
+    shared_ptr<Channel<AVPacketPtr>> src;
+    shared_ptr<Channel<AVFramePtr>> dst;
+    std::map<int, AVCodecContext*> codec_contexts;
+    bool is_stop = false;
 };
