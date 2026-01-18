1eea1f49370c79781aaee6087b732650dfc08523
LimHyunJune <baj2121@naver.com>
2026-01-18 11:55:11 +0900

    feat: ingest module

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "Logger.h"
 
+#include "MediaSource.h"
+
 
 using namespace std;
 
 int main()
 {
+    MediaSourceParam param;
+    param.url = "srt://0.0.0.0:5000?mode=listener&latency=2000&pkt_size=1316&rcvbuf=1073741824";
+
+    MediaSource* ms = new MediaSource();
+    ms->initialize(param, make_unique<Channel<AVPacketPtr>>());
+    ms->run();
+
+
     BOOST_LOG(debug) << "Test";
 }
